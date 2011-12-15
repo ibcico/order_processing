e6750bedce354f993b2cde7c0e59e30a0445d0c1
zhuravlev <i.zhuravlev@ibcico.com>
2011-12-16 01:05:26 +0400

    eletor settings

--- extension.driver.php
+++ extension.driver.php
@@ -52,6 +52,58 @@
 	
 	public function collect_data($context)
 		{
+			$entry_id = $context['entry']->get('id');
+			
+			$articles = $_POST['fields']['article'];
+			$quantity = $_POST['fields']['count'];
+			
+			if(count($articles) > 0 and count($quantity) > 0){
+				$i = 0;
+				foreach($articles as $article) { 
+					$entrymanager = new EntryManager($this);
+					$entry = $entrymanager->create();
+					$entry->set('section_id', 17);
+					$entry->setData(59, array('handle' => $quantity[$i], 'value' => $quantity[$i], 'value_formatted' => $quantity[$i], 'word_count' => 1));
+			    	$entry->setData(60, array('relation_id' => $article));
+					$entry->commit();
+					$id = $entry->get('id');
+					$i++;
+					Symphony::Database()->query("INSERT INTO tbl_entries_data_62 (id, entry_id, relation_id) VALUES (NULL, '$entry_id', '$id')");
+				}
+				$session_id = session_id();
+				$order_id = Symphony::Database()->fetchVar('entry_id', 0, "SELECT entry_id FROM tbl_entries_data_26 WHERE value = '$session_id'");
+				if($order_id){
+					$entrymanager = new EntryManager($this);
+					$entrymanager->delete($order_id);
+				}
+				//Symphony::Database()->query("DELETE FROM tbl_entries_data_28 WHERE id = '$order_id'");
+			}
+			
+			/*	
+			$entrymanager = new EntryManager($this);
+			$entry = $entrymanager->create();
+			$entry->set('section_id', 17);
+	    	$entry->setData(26, array('handle' => $session_id, 'value'=> $session_id, 'value_formatted' => $session_id, 'word_count' => '1'));
+			$entry->commit();
+			$id = $entry->get('id');
+			
+			
+			
+			$id = Symphony::Database()->fetchVar('entry_id', 0, "SELECT entry_id FROM tbl_entries_data_26 WHERE value = '$session_id'");
+			
+			if(!$id){
+				
+			}
+			
+			$subsection = Symphony::Database()->fetchVar('id', 0, "SELECT id FROM tbl_entries_data_28 WHERE entry_id = '$id' AND relation_id = '$field_id'");
+			
+			if(!$subsection){
+				Symphony::Database()->query("INSERT INTO tbl_entries_data_28 (id, entry_id, relation_id, count) VALUES (NULL, '$id', '$field_id', '$count')");
+			}else{
+				Symphony::Database()->query("UPDATE tbl_entries_data_28 SET count = count + $count WHERE entry_id = '$id' AND relation_id = '$field_id'");
+			}
+			
+
 			$position = $this->count;
 			$this->ENT[$this->count] = $context['entry']->get('id');
 			$order_item_id = $context['entry']->get('id');
@@ -72,5 +124,6 @@
 			$_POST['action']['order-event-2'] = 'true';
 			
 			$this->count += 1;
+			*/
 		}
 	}
